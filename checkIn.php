536ad189a018fbc40a909de5231877cff6462e23
Alex <izofatov2007@gmail.com>
2017-07-05 07:33:54 +0300

    first mysql connect))

--- checkIn.php
+++ checkIn.php
@@ -1,12 +1,27 @@
 <?php
 
+    require_once "dbconnect.php";
+
+    $connect = new mysqli($host, $user, $password, $database);
+
+    if ($connect->connect_errno!=0)
+    {
+        echo "Error: ".$connect->connect_errno."Ops..".$connect->connect_errno;
+    }
+    else
+    {
+
     $login = $_POST['login'];
     $passw = $_POST['password'];
+
+    echo "it works!";
 
     echo $login."<br />";
     echo $passw."<br />";
 
+    $connect->close();
 
+    }
 
 
 
---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+
+	$host="localhost";
+	$user="root";
+	$password="mysql";
+	$database="bylo4na";
+	$table="users";
+
+?>
