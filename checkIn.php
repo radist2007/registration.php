ca7aaa1cc427da9f27064eded072ff1b104c10c9
Alex <izofatov2007@gmail.com>
2017-07-05 22:37:38 +0300

    within

--- checkIn.php
+++ checkIn.php
@@ -2,24 +2,35 @@
 
     require_once "dbconnect.php";
 
-    $connect = new mysqli($host, $user, $password, $database);
+    $connect = @new mysqli($host, $user, $password, $database);   // Open connect!  @ - не пускає опис помилки
 
-    if ($connect->connect_errno!=0)
+    if ($connect->connect_errno!=0) // connect error nomber != 0
     {
         echo "Error: ".$connect->connect_errno."Ops..".$connect->connect_errno;
-    }
-    else
-    {
+    } else {
 
-    $login = $_POST['login'];
-    $passw = $_POST['password'];
+        $login = $_POST['login'];
+        $passw = $_POST['password'];
 
-    echo "it works!";
+        $sql = "SELECT * FROM users WHERE name='$login' AND password='$passw'";
 
-    echo $login."<br />";
-    echo $passw."<br />";
+        if ($rezultat = @$connect->query($sql))
+        {
+            $ilu_userow = $rezultat->num_rows;
+            if($ilu_userow>0)
+            {
+                $wiersz = $rezultat->fetch_assoc();     //fetch принеси
+                $user = $wiersz['name'];
 
-    $connect->close();
+
+                $rezultat->free_result();
+                header('Location: within.php');
+            } else {
+
+            }
+        }
+
+        $connect->close();  // Close connect.
 
     }
 
---
+++ within.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>bylo4na</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <form action="checkIn.php" method="post">
+            login: <br /> <input type="text" name="login"><br />
+            password: <br /> <input type="password" name="password"><br /><br /> 
+            <input type="submit" value="ОК">
+        </form>
+
+    </body>
+</html>
