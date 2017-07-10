49ca81ac0063aa232d5947eb816ef9553b8a432b
Alex <izofatov2007@gmail.com>
2017-07-10 06:07:35 +0300

    add w3schools

--- index.php
+++ index.php
@@ -16,22 +16,29 @@
         <meta name="description" content="Тестовий сайт, для перевірки реєстрації. PHP && MySql" />
         <meta name="keywords" content="тест, логін, пароль" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link href="css/style.css" rel="stylesheet">
         <script src="js/jsTimer.js"></script>
     </head>
     <body onload="time()">
 
-        <header class="header">
-            <h1>bylo4na</h1>
+        <header class="w3-bar w3-light-grey w3-border">
+            <!--<a class="w3-bar-item w3-button w3-green">bylo4na</a>-->
+            <a href="#" class="w3-bar-item w3-button w3-green"><i class="fa fa-home"></i></a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-search"></i></a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i></a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-globe"></i></a>
+            <a href="#" class="w3-bar-item w3-button"><i class="fa fa-sign-in"></i></a>
             <!--<h4 id="time"></h4>-->
 
         </header>
 
         <div class="login">
-            <form action="checkIn.php" method="post">
-                login: <input type="text" name="login">
-                password: <input type="password" name="password">
-                <input id="loginSubmit" type="submit" value="ОК">
+            <form class="w3-container" action="checkIn.php" method="post">
+                login: <input class="w3-input w3-border w3-light-grey" type="text" name="login">
+                password: <input class="w3-input w3-border w3-light-grey" type="password" name="password"><br />
+                <input class="w3-btn w3-light-green" id="loginSubmit" type="submit" value="ОК">
             </form>
             <a href="registration.php">registration</a>
         </div>
