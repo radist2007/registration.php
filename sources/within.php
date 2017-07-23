644a8b8633383c08853e1d80569aa9fc7e6ba63b
Alex <izofatov2007@gmail.com>
2017-07-23 22:50:12 +0300

    within.php++

--- sources/within.php
+++ sources/within.php
@@ -18,6 +18,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="../css/style.css" rel="stylesheet">
+        <link href="../css/media.css" rel="stylesheet">
         <script src="../js/jsTimer.js"></script>
     </head>
     <body>
@@ -25,15 +26,19 @@
             <h1>registration</h1>
             <p>in</p>
         </header>
-        <conteiner class="conteiner">
-            <div class="wrap">
-                <h2>wrap</h2>
+
+
+            <section class="container">
+                <div class="wrap">
+                    <div class="login">
+                        <h2>Вітаю, тепер Ви знаходитесь на сайті як зареєстрований користувач!</h2>
+                    </div>
+                </div>
 <?php
     echo "<p>Вітаю ".$_SESSION['user'].'![<a href="logout.php"> Logout</a>]</p>';
     
 ?>
-            </div>
-        </conteiner>
+            </section>
 
     </body>
 </html>
