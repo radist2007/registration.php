5ffa4507e699b9ec72f6376837ada4b331f3adaa
Alex <izofatov2007@gmail.com>
2017-08-02 08:05:18 +0300

    to jaNimer add comments

--- js/jsTimer.js
+++ js/jsTimer.js
@@ -1,23 +1,23 @@
 function time(){
 
-	var today = new Date();
+	var today = new Date();    //Отримуэмо повну інфорамацію, про дату та час - зараз
 
-	var day = today.getDate();
-	var month = today.getMonth()+1;
-	var year = today.getFullYear();
+	var day = today.getDate();       //Отримуємо день
+	var month = today.getMonth()+1;   //Отримуємо місяць
+	var year = today.getFullYear();   //Отримуємо рік
 
-	var hour = today.getHours();
-	if(hour<10) hour = "0"+hour;
+	var hour = today.getHours();   //Отримуємо годину
+	if(hour<10) hour = "0"+hour;   //Додаємо до години 0 якщо вона > 10
 
-	var minute = today.getMinutes();
-	if(minute<10) minute = "0"+minute;
+	var minute = today.getMinutes();     //Отримуємо хвилини
+	if(minute<10) minute = "0"+minute;    //Додаємо до хвилин 0 якщо вони > 10
 
-	var second = today.getSeconds();
-	if(second<10) second = "0"+second;
+	var second = today.getSeconds();      //Отримуємо секунди
+	if(second<10) second = "0"+second;    //Додаємо до секунд 0 якщо вони > 10
 
-	document.getElementById("time").innerHTML = 
-		day+"."+month+"."+year+" | "+hour+":"+minute+":"+second;
+	document.getElementById("time").innerHTML =         //До елементу з id="time" 
+		day+"."+month+"."+year+" | "+hour+":"+minute+":"+second;    //додаємо дату та час
 
-	setTimeout("time()", 1000);
+	setTimeout("time()", 1000);    //Ставимо зартимку в 1 секунду
 
 }
