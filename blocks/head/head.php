dbe4e8d26c42553e9d18e7f88315ff2f38b74d2b
Alex <izofatov2007@gmail.com>
2017-08-07 21:55:23 +0300

    head++

--- blocks/head/head.php
+++ blocks/head/head.php
@@ -8,6 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="../css/style.css" rel="stylesheet">
+        <link href="../css/animate.css" rel="stylesheet">
         <link href="../css/media.css" rel="stylesheet">
         <script src="../js/jsTimer.js"></script>
+        <script src="../js/jsBtn.js"></script>
     </head>
